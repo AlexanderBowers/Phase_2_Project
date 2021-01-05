a09cc6dd88082d2f8641921c8781369138b5c957
Rmiverson <Rmiverson99@gmail.com>
2021-01-05 09:17:56 -0800

    edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 <<<<<<< HEAD
+
+
+
+
+
+
+
+
+
+
+
+
+
 User.delete_all
 Group.delete_all
 Raid.delete_all
