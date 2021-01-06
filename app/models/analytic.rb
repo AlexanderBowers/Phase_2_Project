0df70d2937ca1e11574b84c49d6683be2362fa83
AlexanderBowers <7319abowers@gmail.com>
2021-01-05 20:58:16 -0800

    added more method models for analytics and users

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -1,5 +1,6 @@
 class Analytic < ApplicationRecord
 
+
     def groups
         "Current amount of groups made: #{Group.all.length}"
     end
@@ -12,19 +13,57 @@ class Analytic < ApplicationRecord
         "Current amount of raids made: #{Raid.all.length}"
     end
 
+    def total_raids
+        total = 0
+        Group.all.each do |g|
+            if g.finished == true
+                total += g.raids.length
+            end
+        end
+        "Current amount of raids finished: #{total}"
+    end
+
     def roles
         "There are currently #{UserGroupRole.all.length} roles assigned to groups"
     end
 
+    def tanks
+        total = 0
+        Group.all.each do |g|
+            if g.finished == true
+                total += g.party[:tank].length
+            end
+        end
+        "Out of all groups who have finished, there have been #{total} people as tanks."
+    end
+
+    def dps
+        total = 0
+        Group.all.each do |g|
+            if g.finished == true
+                total += g.party[:dps].length
+            end
+        end
+        "Out of all groups who have finished, there have been #{total} people as dps."
+    end
+
+    def healers
+        total = 0
+        Group.all.each do |g|
+            if g.finished == true
+                total += g.party[:healer].length
+            end
+        end
+        "Out of all groups who have finished, there have been #{total} people as healer."
+    end
+
+
+
+   
+
+    
+
 
 
 end
-# analytics
-# counter for groups made
-# counter for users made
-# counter for raids made
-# counter for raid finished most often
-# raid with most tanks
-# raid with most dps
-# raid with most healers
-# counter for user_group_roles
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,69 @@ class User < ApplicationRecord
         self.inventory.delete(item)
         self.save
     end
+
+    def total_raids
+        total = 0
+        self.groups.all.each do |g|
+            if g.finished == true
+                total += g.raids.length
+            end
+        end
+        "#{self.username} has participated in #{total} raids."
+    end
+
+    def total_groups
+        "#{self.username} has participated in #{self.groups.all.length} groups."
+    end
+
+    def party_leads
+        total = 0
+        self.groups.all.each do |g|
+            if g.finished == true && g.party_leader == self
+                total += 1
+            end
+        end
+        "#{self.username} has been the party leader of #{total} groups."
+    end
+
+    def tank_count
+        total = 0
+        groups = User.all[6].user_group_roles.all.where(role_id: 1)
+        groups.each do |g|
+            if g.group.finished == true
+                total += 1
+            end
+        end
+        "#{self.username} has been a tank #{total} time(s)."
+    end
+
+    def dps_count
+        total = 0
+        groups = User.all[6].user_group_roles.all.where(role_id: 2)
+        groups.each do |g|
+            if g.group.finished == true
+                total += 1
+            end
+        end
+        "#{self.username} has been a dps #{total} time(s)."
+    end
+
+    def healer_count
+        total = 0
+        groups = User.all[6].user_group_roles.all.where(role_id: 3)
+        groups.each do |g|
+            if g.group.finished == true
+                total += 1
+            end
+        end
+        "#{self.username} has been a healer #{total} time(s)."
+    end
+
+#     users track how many raids they've done
+# users track how many groups they've been in and finished
+
+# how many times a user has been party lead and finished a group
+# how many times a user has done tank
+# has done dps
+# has done healer
 end
