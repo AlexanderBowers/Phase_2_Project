f7b7051ec9c33904caef63748b601a38c1c11969
Rmiverson <Rmiverson99@gmail.com>
2021-01-06 09:32:50 -0800

    finished navbar login integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,6 @@ class ApplicationController < ActionController::Base
         @current_user ||= User.find(session[:user_id])
     end
 
-    def logged_in_true
-        !current_user.nil?
-    end
-
     helper_method :current_user
     
 end
