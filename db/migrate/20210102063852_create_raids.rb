6b123c003910bdd4cf33d420c946f7e381267ff0
Rmiverson <Rmiverson99@gmail.com>
2021-01-06 16:32:04 -0800

    more styles for user page

--- db/migrate/20210102063852_create_raids.rb
+++ db/migrate/20210102063852_create_raids.rb
@@ -2,6 +2,7 @@ class CreateRaids < ActiveRecord::Migration[6.1]
   def change
     create_table :raids do |t|
       t.string :name
+      t.string :raid_desc
       t.string :reward_item_1
       t.string :reward_item_2
       t.boolean :completed, :default => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    Role.destroy_all
+    User.destroy_all
+    Group.destroy_all
+    Raid.destroy_all
+    UserGroup.destroy_all
+    UserGroupRole.destroy_all
+    GroupRaid.destroy_all
 
+    admin = User.create(:username => "Admin", :password => "lol")
+    u1 = User.create(:username => "TSnuttle", :email => "lol@gmail.com", :discord_id => "TSnuttle#6461", :steam_id => "tsnuttle", :password => "lol", :user_desc => "Hi, I play games.")
+
+    r1 = Raid.create(:name => "Raid on " + Faker::Games::ElderScrolls.city)
+    r2 = Raid.create(:name => "Attack on " + Faker::Games::ElderScrolls.city)
+    r3 = Raid.create(:name => "Raid " + Faker::Games::ElderScrolls.city)
 
     Role.create(name: "tank")
     Role.create(name: "dps")
